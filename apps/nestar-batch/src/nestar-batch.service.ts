0d54442786eafec1d6718c284b4f73f1a01b6032
davlatjanov <ndavlatjonov@gmail.com>
2025-12-24 15:46:00 +0900

    fix: modify monorepo project

--- apps/nestar-batch/src/nestar-batch.service.ts
+++ apps/nestar-batch/src/nestar-batch.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class NestarBatchService {
   getHello(): string {
-    return 'Hello World!';
+    return 'WELCOME NESTAR-BATCH!';
   }
 }
