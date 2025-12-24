3dc1a54ffe789dd60ad523562e0bc21d7efbecfe
davlatjanov <ndavlatjonov@gmail.com>
2025-12-24 16:08:50 +0900

    checking

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { PropertyModule } from './property/property.module';
-
+/*Just checking*/
 @Module({
-  imports: [MemberModule, PropertyModule]
+	imports: [MemberModule, PropertyModule],
 })
 export class ComponentsModule {}
