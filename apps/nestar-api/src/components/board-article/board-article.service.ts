3486a9dcd2a16d6af0cbbd330631cc3896a3b502
davlatjanov <ndavlatjonov@gmail.com>
2026-01-12 16:49:37 +0900

    feat: develop createBoardArticle GraphQL API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
 
 @Injectable()
 export class BoardArticleService {
@@ -12,4 +14,20 @@ export class BoardArticleService {
 		private readonly memberService: MemberService,
 		private readonly viewService: ViewService,
 	) {}
+
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberArticles',
+				modifier: 1,
+			});
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }
