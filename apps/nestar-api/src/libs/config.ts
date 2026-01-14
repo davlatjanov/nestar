fefb6dce18f39fc688203471e2a5d93ef07a0ccb
davlatjanov <ndavlatjonov@gmail.com>
2026-01-14 16:34:35 +0900

    feat: integrate comment retaled DTOs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -13,6 +13,8 @@ export const availablePropertySorts = [
 ];
 export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
+export const availableCommentSorts = ['createdAt', 'updatedAt'];
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
